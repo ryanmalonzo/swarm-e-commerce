20cca8c0eed82cefd7a74d2438c7b72a32e7ddfc
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2024-12-22 18:33:40 +0100

    Initial commit

---
+++ api/index.ts
@@ -0,0 +1,16 @@
+import express, { Response } from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT
+
+app.get('/', (_, res: Response) => {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
+
