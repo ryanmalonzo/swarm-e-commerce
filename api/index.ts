f5e4aa5f3627b4fcfa30f6b081ffe5772075340a
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2024-12-23 11:22:11 +0100

    Add order endpoints

--- api/index.ts
+++ api/index.ts
@@ -31,6 +31,40 @@ app.post("/login", async (req: Request, res: Response) => {
   res.status(200).json(user);
 });
 
+app.post("/orders", async (req: Request, res: Response) => {
+  const { userId, products } = req.body;
+  const order = await db.insert(schema.orders).values({
+    userId,
+  }).returning();
+
+  products.forEach(async (product: typeof schema.products.$inferSelect) => {
+    await db.insert(schema.productsToOrders).values({
+      orderId: order[0].id,
+      productId: product.id,
+    });
+  });
+
+  res.sendStatus(201);
+});
+
+app.get("/orders", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  res.status(200).json(
+    await db.query.orders.findMany({
+      where: (orders, { eq }) => eq(orders.userId, Number(id)),
+    }),
+  );
+});
+
+app.get("/orders/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  res.status(200).json(
+    await db.query.orders.findFirst({
+      where: (orders, { eq }) => eq(orders.id, Number(id)),
+    }),
+  );
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
